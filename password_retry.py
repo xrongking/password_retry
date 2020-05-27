053b67515d2d86871e31a58d62766af10706571d
Sanya Yang <xrongking@gmail.com>
2020-05-27 17:11:29 +0800

    version update

--- password_retry.py
+++ password_retry.py
@@ -1,10 +1,14 @@
 password = 'a123456'
 count = 3
 while count > 0:
+	count = count - 1
 	get_password = input('請輸入密碼：')
 	if get_password == password:
 		print('登入成功！')
 		break
 	else:
-		count = count - 1
-		print('密碼錯誤！還有', count, '次機會！')
+		print('密碼錯誤！')
+		if count > 0:
+			print('你還有', count, '次機會！')
+		else:
+			print('你沒機會了！')
